8b565c1facd194304922bd8feea8f9bda7fa8e4f
devkjoon <devkjoon@gmail.com>
2023-01-03 16:34:31 -0500

    properly routed db.json file to apiroutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,7 +6,7 @@ let uniqid = require('uniqid');
 module.exports = (app) => {
 
   app.get('/api/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './db/db.json'));
+    res.sendFile(path.join(__dirname, '../db/db.json'));
   });
 
   app.post('/api/notes', (req, res) => {
