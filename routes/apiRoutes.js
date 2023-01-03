ed64eb41e45fd89a1cc1e5337fcc684d51a07aba
devkjoon <devkjoon@gmail.com>
2023-01-03 15:15:12 -0500

    separated the routes into two js files, added uuid

---
+++ routes/apiRoutes.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+
+const { v4: uuidv4 } = require('uuid');
+
+module.exports = function (app) {
+    app.get('/api/notes', (req, res) => {
+        let data = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+        res.json(data);
+    });
+
+    app.post("/api/notes", (req, res) => {
+        const newNote = req.body;
+        newNote.id = uuidv4();
+        let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+        data.push(newNote);
+        fs.writeFileSync('./db/db.json', JSON.stringify(data));
+        res.json(data);
+    });
+
+    app.delete("/api/notes/:id", (req, res) => {
+        let noteId = req.params.id.toString();
+        let data = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+        const newData = data.filter( note => note.id.toString() !== noteId );
+        fs.writeFileSync('./db/db.json', JSON.stringify(newData));
+        res.json(newData);
+    });
+}
--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const express = require('express');
-const fs = require('fs');
-const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
-app.use(express.static(__dirname));
+app.use(express.static('public'));
 
-require('./routes/routes')(app);
+require('./routes/apiRoutes')(app);
+require('./routes/htmlRoutes')(app);
 
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
