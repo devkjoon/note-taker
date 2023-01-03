d9ed58ccce3d707f68f5ae72479e41e4578292ec
devkjoon <devkjoon@gmail.com>
2023-01-03 11:53:06 -0500

    completed setting up routes and functionalities

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express');
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+const fs = require('fs');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
-app.use(express.static('public'));
-app.use('/api', apiRoutes);
-app.use('/api', htmlRoutes);
+app.use(express.static(__dirname));
 
-app.listen(PORT, () => console.log(`Port ${PORT} activated!`));
+require('./routes/routes')(app);
+
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
